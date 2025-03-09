b74dfa337920802be8cb9c0e680c4d67786e5a34
chen-yu <2996705738@qq.com>
2025-03-10 02:29:59 +0800

    12345

---
+++ Decorator/Client.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+public class Client {
+    public static void main(String[] args) {
+        IBirthdayCake birthdayCake=new Cake();
+        birthdayCake.show();
+        Cream cream=new Cream(birthdayCake);
+        cream.putCream();
+        Fruit fruit=new Fruit(birthdayCake);
+        fruit.putFruit();
+        fruit.show();
+    }
+}
---
+++ Decorator/Cream.java
@@ -0,0 +1,10 @@
+package Decorator;
+
+public class Cream extends Decorating{
+    public Cream(IBirthdayCake birthdayCake) {
+        super(birthdayCake);
+    }
+    public void putCream(){
+        System.out.println("...");
+    }
+}
